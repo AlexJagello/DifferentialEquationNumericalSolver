e1925ee597faad63af3540cdeaa796bc9c9132e5
unknown <sascha.yagello@yandex.ru>
2023-02-27 23:17:25 +0300

    Initial commit

---
+++ DifferentialEquationNumerical/DifferentialEquationNumerical/PythonCalculation.cs
@@ -0,0 +1,39 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferentialEquationNumerical
+{
+    public static class PythonCalculationClass
+    {
+        private static readonly ScriptEngine engine;
+        private static readonly ScriptScope scope;
+
+        static PythonCalculationClass()
+        {
+            engine = Python.CreateEngine();
+            scope = engine.CreateScope();
+        }
+
+        public static double PythonCalculation(string expression, double x)
+        {
+            scope.SetVariable("x", x);
+            engine.Execute("from math import *\n" + expression, scope);
+            dynamic res = scope.GetVariable("y");
+            return Convert.ToDouble(res);
+        }
+
+        public static double PythonCalculation_DifferentialEquation(string expression, double x, double y)
+        {
+            scope.SetVariable("x", x);
+            scope.SetVariable("y", y);
+            engine.Execute("from math import *\n" + expression, scope);
+            dynamic res = scope.GetVariable("dy");
+            return Convert.ToDouble(res);
+        }
+    }
+}
---
+++ DifferentialEquationNumerical/DifferentialEquationNumerical/SimpleEquation.cs
@@ -0,0 +1,91 @@
+﻿using DifferentialEquationNumerical.Models;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferentialEquationNumerical
+{
+    public static class SimpleEquation
+    {
+
+        public static void CalculateSimpleEquation(string expression,ref double[] x, ref double[] y, double x0, double step, int stepAmounts, Func<string, double, double> calculateExpression)
+        {
+            for (int i = 0; i < stepAmounts; i++)
+            {
+                var xi = x0 + i * step;
+                x[i] = xi;
+                y[i] = calculateExpression(expression, xi);
+            }
+        }
+
+        public static double[,] CalculateSimpleEquation_ArrayReturn(string expression, double x0, double step, int stepAmounts, Func<string, double, double> calculateExpression)
+        {
+            var resultMass = new double[2, stepAmounts];
+
+            for(int i = 0; i < stepAmounts; i++)
+            {
+                var xi = x0 + i * step;
+                resultMass[0, i] = xi;
+                resultMass[1, i] = calculateExpression(expression, xi);
+            }
+
+            return resultMass;
+        }
+
+        public static IPoint[] CalculateSimpleEquation(string expression, double x0, double step, int stepAmounts, Func<string, double, double> calculateExpression)
+        {
+
+            var resultMass = new Point[stepAmounts];
+
+            for (int i = 0; i < stepAmounts; i++)
+            {
+                var xi = x0 + i * step;
+
+                var yi = calculateExpression(expression, xi);
+
+                resultMass[i] = new Point(xi, yi);
+               
+            }
+
+            return resultMass;
+        }
+
+
+        public static IEnumerable<IPoint> CalculateSimpleEquation_EnumerableReturn(string expression, double x0, double step, int stepAmounts, Func<string, double, double> calculateExpression)
+        {
+            var resultMass = new System.Collections.ObjectModel.ObservableCollection<IPoint>();
+
+            for (int i = 0; i < stepAmounts; i++)
+            {
+                var xi = x0 + i * step;
+
+                var yi = calculateExpression(expression, xi);
+
+                resultMass.Add(new Point(xi, yi));
+
+            }
+
+            return resultMass;
+        }
+
+
+
+        public static async Task<double[,]> CalculateSimpleEquationAsync_ArrayReturn(string expression, double x0, double step, int stepAmounts, Func<string, double, double> calculateExpression)
+        {
+            return await Task.Run(() => CalculateSimpleEquation_ArrayReturn(expression, x0, step, stepAmounts, calculateExpression));
+        }
+
+        public static async Task<IPoint[]> CalculateSimpleEquationAsync(string expression, double x0, double step, int stepAmounts, Func<string, double, double> calculateExpression)
+        {
+            return await Task.Run(() => CalculateSimpleEquation(expression, x0, step, stepAmounts, calculateExpression));
+        }
+
+
+     
+
+    }
+}
